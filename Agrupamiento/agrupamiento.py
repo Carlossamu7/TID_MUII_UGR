206fdcc3cd33fc8016c826816a68bcfdb89f7a97
Carlos <carlossamu7@gmail.com>
2020-11-24 18:12:26 +0100

    Closes #10 kmeans y comparación de clusters de la primera columna

--- Agrupamiento/agrupamiento.py
+++ Agrupamiento/agrupamiento.py
@@ -20,6 +20,8 @@ from matplotlib import pyplot as plt
 from sklearn.cluster import AgglomerativeClustering
 import numpy as np
 from scipy import stats
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
 
 IMPRIME_INFO = True     # Indica si imprimir información
 
@@ -70,6 +72,38 @@ def delete_outliers(df):
     print("Número de outliers borrados: {}".format(len(df)-len(df_outliers)))
     return df_outliers
 
+""" Aplica el clustering KMeans. Devuelve el clasificador y las etiquetas predichas.
+- X: datos.
+"""
+def get_k_medias(X):
+    kmeans = KMeans(n_clusters=3).fit(X)
+    labels = kmeans.predict(X)
+    print()
+    print(kmeans)
+    print("\nCentroides:")
+    print(kmeans.cluster_centers_)
+    print("\nEtiquetas después de kmeans:")
+    print(labels)
+    return kmeans, labels
+
+""" Aplica el clustering KMeans. Devuelve el clasificador y las etiquetas predichas.
+- X: datos.
+- kmeans: clasificador kmedias ya entrenado.
+- labels: etiquetas predichas.
+"""
+def plot_cluster_primera_variable(X, kmeans, labels):
+    print("\nAgrupación de clusters respecto de la primera variable...")
+    plt.scatter(X[labels  == 0]["Alcohol"], X[labels  == 0]["Alcohol"], s=10,c='red', label='Class 1')
+    plt.scatter(X[labels  == 1]["Alcohol"], X[labels  == 1]["Alcohol"], s=10,c='green', label='Class 2')
+    plt.scatter(X[labels  == 2]["Alcohol"], X[labels  == 2]["Alcohol"], s=10,c='purple', label='Class 3')
+    plt.scatter(kmeans.cluster_centers_[0][0], kmeans.cluster_centers_[0][0], marker="*", s=100, c='red', label='Centroide 1')
+    plt.scatter(kmeans.cluster_centers_[1][0], kmeans.cluster_centers_[1][0], marker="*", s=100, c='green', label='Centroide 2')
+    plt.scatter(kmeans.cluster_centers_[2][0], kmeans.cluster_centers_[2][0], marker="*", s=100, c='purple', label='Centroide 3')
+    plt.legend()
+    plt.title("Clusters de clases")
+    plt.gcf().canvas.set_window_title("Práctica 2 - Agrupamiento")
+    plt.show()
+
 
 ########################
 #####     MAIN     #####
@@ -91,6 +125,12 @@ def main():
     df_o = delete_outliers(df)
     X_o, y_o =split_data(df_o)
     dendograma(X_o, "Dendograma sin outliers")
+
+    kmeans, labels = get_k_medias(X_o)
+    # Comparamos la distribución de las clases respecto a la primera columna.
+    plot_cluster_primera_variable(X_o, kmeans, labels)
+    silhouette_avg3 = silhouette_score(X_o, (kmeans.labels_), metric='euclidean')
+    print("\nLa puntuación de silhouette es: {}".format(silhouette_avg3))
 
 
 if __name__ == "__main__":
